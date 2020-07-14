dcb05ed30ef4bc6195678441eee08843e869cfd7
berlink2 <berlink.k2@gmail.com>
2020-07-14 12:10:52 +0700

    Updates

---
+++ precache-manifest.eba10796a23ab393338103373579b10f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd84d56d3d3ff7b341b146167fd2d864",
+    "url": "/cermati_test/index.html"
+  },
+  {
+    "revision": "7caab7d3f4b044d95225",
+    "url": "/cermati_test/static/css/2.5f1c0dbe.chunk.css"
+  },
+  {
+    "revision": "a7b692f17044111868c7",
+    "url": "/cermati_test/static/css/main.9d137aa6.chunk.css"
+  },
+  {
+    "revision": "7caab7d3f4b044d95225",
+    "url": "/cermati_test/static/js/2.f946e807.chunk.js"
+  },
+  {
+    "revision": "c3661c076671f579c9e33712f39598f0",
+    "url": "/cermati_test/static/js/2.f946e807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b692f17044111868c7",
+    "url": "/cermati_test/static/js/main.017ac5e0.chunk.js"
+  },
+  {
+    "revision": "50d4712346eec13104e8",
+    "url": "/cermati_test/static/js/runtime-main.b2f52c80.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cermati_test/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cermati_test/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cermati_test/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cermati_test/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cermati_test/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a5a74b4ff9be5b81b4ee10c0f456d8e9",
+    "url": "/cermati_test/static/media/work-desk__dustin-lee.a5a74b4f.jpg"
+  }
+]);
